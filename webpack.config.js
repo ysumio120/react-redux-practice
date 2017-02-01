cd93ef6ef511bbb88411995641a7a2be80c0b274
Yuuta Sumioka <ysumio120@yahoo.com>
2017-02-01 04:11:52 -0500

    Basic folder structure and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/js/app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+          //plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/src/js",
+    publicPath: "/src",
+    filename: "bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+};
