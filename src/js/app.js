c180cc17fbb086d8972a66390e6af36fbd19015b
Yuuta Sumioka <ysumio120@yahoo.com>
2017-02-23 21:49:14 -0500

    animate resize

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
-import Route from "./components/Root"
+import Root from "./components/Root"
 import store from "./store"
 
-const Root = document.getElementById('root')
-
-ReactDOM.render(<Route />, Root);
+ReactDOM.render(<Root />, document.getElementById('root'));
