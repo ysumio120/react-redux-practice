7304e0ea0dd67f5aa56fadb520fc41d82dbe7bc3
Yuuta Sumioka <ysumio120@yahoo.com>
2017-04-17 21:33:23 -0400

    testing localStorage (for firefox)

--- src/js/components/User.js
+++ src/js/components/User.js
@@ -31,7 +31,7 @@ class User extends React.Component {
     return queryObj;
   }
 
-  componentWillMount() {
+  componentDidMount() {
     let token = localStorage.getItem("accessToken");
     //console.log(token);
 
